0697d7dffe97471c83f2fd0ecec45d935d363edb
Drikus Roor <drikusroor@gmail.com>
2024-01-27 15:08:05 +0100

    Add Bun configuration files and initial project setup with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  purge: ['./public/**/*.html', './src/**/*.php'], 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
