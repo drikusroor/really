8f2fd678adcd48b3ffa9bba09400ac2032420f41
Drikus Roor <drikusroor@gmail.com>
2024-02-01 10:11:06 +0100

    feat: Fix page generation and add index

--- src/Service/ManagePageService.php
+++ src/Service/ManagePageService.php
@@ -20,7 +20,7 @@ class ManagePageService {
             $date = \date('Y-m-d');
             $slug = $date . '-' . strtolower($frontmatter->getTitle());
         }
-        
+
         $filename = $slug . '.md';
         $filepath = __DIR__ . '/../../content/pages/' . $filename;
 
@@ -29,7 +29,42 @@ class ManagePageService {
         fwrite($file, $content);
         fclose($file);
 
+        $this->generateIndex();
+
         return $slug;
     }
 
+    private function generateIndex() {
+        $pages = scandir(__DIR__ . '/../../content/pages');
+        $pages = array_diff($pages, ['.', '..']);
+        $pages = array_map(function ($page) {
+            return str_replace('.md', '', $page);
+        }, $pages);
+        $pages = array_values($pages);
+
+        
+        $frontmatter = new Frontmatter('Pages', null, 'pages', null, null, true, 'page', null, 'This is an index of all pages.');
+        $content = $this->getPagelistMarkdown($pages);
+        
+        $filename = 'pages.md';
+        $filepath = __DIR__ . '/../../content/pages/' . $filename;
+        
+        $file = fopen($filepath, 'w');
+        fwrite($file, $frontmatter);
+        fwrite($file, $content);
+        fclose($file);
+
+        return true;
+    }
+
+    private function getPagelistMarkdown($pages): string {
+        $markdown = '';
+
+        foreach ($pages as $page) {
+            $markdown .= '- [' . $page . '](/' . $page . ')
+';
+        }
+
+        return $markdown;
+    }
 }
