da3e3cd5fae9a32ff1d7e01c6c9422e8523cb794
Drikus Roor <drikusroor@gmail.com>
2024-02-02 18:07:10 +0100

    Add index.php file with basic HTML structure and content

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Really</title>
+    <link rel="stylesheet" href="/assets/css/index.css">
+</head>
+
+<body class="prose mx-auto min-h-screen flex flex-col">
+    <header class="p-3">
+        <h1 class="text-center">Really</h1>
+    </header>
+
+    <main class="flex-grow p-3">
+        <p>Really is a simple, flat-file CMS written in PHP.</p>
+
+        <p>However, if you see this page, it means that your .htaccess file is not configured correctly.</p>
+    </main>
+
+    <footer class="text-center p-3">
+        <div class="container">
+            <p>Really &copy; <?= date('Y') ?></p>
+        </div>
+    </footer>
+</body>
+
