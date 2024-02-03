fe81b5962f37d6e1ca7fd05b2527ac38e235f642
Drikus Roor <drikusroor@gmail.com>
2024-02-03 17:19:19 +0100

    Update CSS styles and base template***

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,20 @@ export default {
     './index.{php,html}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lemon': {
+          100: '#FFF9C4', // Lighter lemon for backgrounds
+          500: '#FCD34D', // Default lemon for elements
+          700: '#F59E0B', // Darker lemon for accents
+        },
+        'orange': {
+          100: '#FFE5D0', // Lighter orange for backgrounds
+          500: '#FB923C', // Default orange for elements
+          700: '#EA580C', // Darker orange for accents
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
